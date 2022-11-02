f8311d420fe284d71f53d86cfd6912f0c4c61505
May726 <cist.b220.m.sudou@gmail.com>
2022-11-02 11:31:02 +0900

    課題1-3

---
+++ src/lecture01/Kadai12.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class Kadai12 {
+    public static void main(String[] args) {
+        int studentNumber = 2201400;
+        System.out.println("B" + studentNumber + " Sudou Mayu");
+    }
+}
---
+++ src/lecture01/Kadai13.java
@@ -0,0 +1,12 @@
+package lecture01;
+
+public class Kadai13 {
+    public static void main(String[] args) {
+        int age = 22;
+        if(age < 20){
+            System.out.println("私、" + age + "歳だからお酒飲めない！");
+        }else {
+            System.out.println("私、" + age + "歳だからお酒飲める！");
+        }
+    }
+}
